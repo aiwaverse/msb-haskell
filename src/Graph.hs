e625b2a00655757539f9d0fd584a2625e3c924c4
aiwa <aiwavision@protonmail.com>
2019-11-09 18:08:09 -0300

    some comments

--- src/Graph.hs
+++ src/Graph.hs
@@ -30,9 +30,11 @@ defaultGraph n = Graph { vertices      = generateDefaultVert n
                        , linesToAvoid  = []
                        }
 
+-- simply de-indexes an indexed list
 unindexed :: [(Int,a)] -> [a]
 unindexed = map snd
 
+--two functions to generate a default graph given n dimensions
 generateDefaultVert :: Int -> IndexedIntList
 generateDefaultVert dim = indexed . map indexed $ generateDefaultVertLoop dim dim
 
@@ -41,13 +43,16 @@ generateDefaultVertLoop _ 0 = []
 generateDefaultVertLoop dim n =
     (take dim . cycle $ [-2]) : generateDefaultVertLoop dim (n - 1)
 
+--given vertices and the list of valid lines, return the vertices that have indexes in those lines
 validLines :: IndexedIntList -> [Int] -> IndexedIntList
 validLines vert lines = filter (\e->fst e `elem` lines) vert
 
+--given a list of indexs to lit and an indexes list, return the same list without those indexes
 deleteAtList :: [Int] -> [(Int,a)] -> [(Int,a)]
 deleteAtList [x] vert = deleteAt x vert
 deleteAtList (x:xs) vert = deleteAt x (deleteAtList xs vert)
 
+--given a list of indexes and an indexed int list, will remove the internal indexes 
 deleteCollums :: [Int] -> IndexedIntList -> [[(Int,Int)]]
 deleteCollums col = map (deleteAtList col) . unindexed
 
